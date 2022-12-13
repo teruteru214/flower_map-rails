0db5f0f7f14840e4d21fbe4e8779840952fc4a34
teruteru <tutituti3080@gmail.com>
2022-12-13 17:49:54 +0900

    Add: flash_messageの追加

--- app/controllers/user_sessions_controller.rb
+++ app/controllers/user_sessions_controller.rb
@@ -4,14 +4,15 @@ class UserSessionsController < ApplicationController
   def create
     @user = login(params[:email], params[:password])
     if @user
-      redirect_back_or_to root_path
+      redirect_back_or_to root_path, success: "ログインが完了！"
     else
+      flash.now[:danger] = "ログインが失敗"
       render :new
     end
   end
 
   def destroy
     logout
-    redirect_to root_path
+    redirect_to root_path, success: "ログアウト完了！"
   end
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,8 +6,9 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
-      redirect_to login_path
+      redirect_to login_path, success: "ユーザー作成が完了！"
     else
+      flash.now[:danger] = "ユーザ作成失敗"
       render :new
     end
   end
