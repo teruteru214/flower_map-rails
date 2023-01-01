3cc575ccb53fdf7ede59f7a9809b96171a24c16a
teruteru <tutituti3080@gmail.com>
2023-01-01 09:55:41 +0900

    Add: モデルとコントローラーとルーティングの追加

--- app/controllers/flowers_controller.rb
+++ app/controllers/flowers_controller.rb
@@ -4,4 +4,28 @@ class FlowersController < ApplicationController
   def index
     @flowers = Flower.all.includes(:user).order(created_at: :desc)
   end
+
+  def new
+    @flower = Flower.new
+  end
+
+  def created
+    @flower = current_user.flower.build(flower_params)
+    if @flower.save
+      redirect_to flowers_path, success: "投稿完了"
+    else
+      flash.now['danger'] = "投稿失敗"
+      render :new
+    end
+  end
+
+  def show
+    @flower = Flower.find(params[:id])
+  end
+
+  private
+
+  def flower_params
+    params.require(:flower).permit(:name, :address, :datetime,:status)
+  end
 end
