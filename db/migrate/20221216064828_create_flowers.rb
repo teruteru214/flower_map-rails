63eb250d3c3db1071277fffc1d37207e7390616e
teruteru <tutituti3080@gmail.com>
2023-01-01 19:28:04 +0900

    Fix: 緯度経度の制約の変更

--- db/migrate/20221216064828_create_flowers.rb
+++ db/migrate/20221216064828_create_flowers.rb
@@ -2,7 +2,7 @@ class CreateFlowers < ActiveRecord::Migration[7.0]
   def change
     create_table :flowers do |t|
       t.string :name, null: false
-      t.time :datetime, null: false
+      t.datetime :datetime, null: false
       t.integer :status, null: false, defalut: 0
       t.string :flower_word
       t.string :birth_flower_day
