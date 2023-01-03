dd72ec02fc0e6b5b448075b395dc06c6edc533e9
teruteru <tutituti3080@gmail.com>
2023-01-03 18:00:49 +0900

    Add: minimagickによる画像のresizeの設定を追加

--- app/uploaders/flower_image_uploader.rb
+++ app/uploaders/flower_image_uploader.rb
@@ -1,7 +1,7 @@
 class FlowerImageUploader < CarrierWave::Uploader::Base
   # Include RMagick or MiniMagick support:
   # include CarrierWave::RMagick
-  # include CarrierWave::MiniMagick
+  include CarrierWave::MiniMagick
 
   # Choose what kind of storage to use for this uploader:
   storage :file
@@ -19,15 +19,15 @@ class FlowerImageUploader < CarrierWave::Uploader::Base
   end
 
   # Process files as they are uploaded:
-  # process scale: [200, 300]
-  #
+  process resize_to_fill: [350, 350, gravity='Center']
+
   # def scale(width, height)
   #   # do something
   # end
 
   # Create different versions of your uploaded files:
-  # version :thumb do
-  #   process resize_to_fit: [50, 50]
+  # version :index do
+  #   process resize_to_fill: [350, 350, gravity='Center']
   # end
 
   # Add an allowlist of extensions which are allowed to be uploaded.
