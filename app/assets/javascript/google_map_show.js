7bfe29fa6d8b5726654412a28d5531a214e7e6eb
teruteru <tutituti3080@gmail.com>
2023-01-11 09:41:16 +0900

    Fix: 花の詳細画面に地図のMarkerを表示するように修正

--- app/assets/javascript/google_map_show.js
+++ app/assets/javascript/google_map_show.js
@@ -8,8 +8,13 @@ function initMap(){
   geocoder = new google.maps.Geocoder()
 
   map = new google.maps.Map(document.getElementById('map'), {
-    center:  {lat: 35.6803997, lng:139.7690174},  //東京
+    center:  {lat: 35.6803997, lng: 139.7690174},  //東京
     zoom: 16,
 
+  });
+
+  marker = new google.maps.Marker({
+    position:  {lat: 35.6803997, lng: 139.7690174 },
+    map: map
   });
 }
