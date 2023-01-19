9951885af32f17e9db9ebc231c028391eb623009
teruteru <tutituti3080@gmail.com>
2023-01-19 16:47:32 +0900

    Add: プライバシーポリシーと利用規約のルーティングを作成

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   root 'static_pages#top'
 
+  get '/privacy', to: 'static_pages#privacy'
+  get '/teams', to: 'static_pages#teams'
   get 'login', to: 'user_sessions#new'
   post 'login', to: 'user_sessions#create'
   delete 'logout', to: 'user_sessions#destroy'
