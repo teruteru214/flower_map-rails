a59270b75d7af8cd63759c5172f52b010d3b20f2
teruteru <tutituti3080@gmail.com>
2023-02-03 09:12:42 +0900

    Add: Unknown_flowerモデルを追加

---
+++ db/migrate/20230203000242_create_unknown_flowers.rb
@@ -0,0 +1,10 @@
+class CreateUnknownFlowers < ActiveRecord::Migration[7.0]
+  def change
+    create_table :unknown_flowers do |t|
+      t.string :feature
+      t.integer :reception, null:false, defaults: 0
+
+      t.timestamps
+    end
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_02_02_002850) do
+ActiveRecord::Schema[7.0].define(version: 2023_02_03_000242) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -65,6 +65,13 @@ ActiveRecord::Schema[7.0].define(version: 2023_02_02_002850) do
     t.string "address_detail"
     t.string "flower_image"
     t.index ["user_id"], name: "index_flowers_on_user_id"
+  end
+
+  create_table "unknown_flowers", force: :cascade do |t|
+    t.string "feature"
+    t.integer "reception", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "users", force: :cascade do |t|
