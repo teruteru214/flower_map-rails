aa885f52d241156e812c7e8e0a26db69ce2e5ac9
teruteru <tutituti3080@gmail.com>
2023-02-07 14:34:34 +0900

    Remove: 花言葉のカラムを削除

--- db/schema.rb
+++ db/schema.rb
@@ -55,13 +55,6 @@ ActiveRecord::Schema[7.0].define(version: 2023_02_05_065258) do
     t.index ["user_id"], name: "index_favorites_on_user_id"
   end
 
-  create_table "flower_words", force: :cascade do |t|
-    t.string "name"
-    t.string "word"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
-
   create_table "flowers", force: :cascade do |t|
     t.string "name", null: false
     t.date "date", null: false
