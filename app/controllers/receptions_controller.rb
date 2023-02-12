c93996f0968c28bfc20a4ae603d2738cab58ae51
teruteru <tutituti3080@gmail.com>
2023-02-12 17:17:11 +0900

    Fix: 回答受付のコントローラの修正

--- app/controllers/receptions_controller.rb
+++ app/controllers/receptions_controller.rb
@@ -2,12 +2,10 @@ class ReceptionsController < ApplicationController
   def create
     unknown_flower = UnknownFlower.find(params[:unknown_flower_id])
     current_user.reception(unknown_flower)
-    redirect_back fallback_location: unknown_flowers_path, success: t('.success')
   end
 
   def destroy
     unknown_flower = current_user.receptions.find(params[:id]).unknown_flower
     current_user.unreception(unknown_flower)
-    redirect_back fallback_location: unknown_flowers_path, success: t('.success')
   end
 end
