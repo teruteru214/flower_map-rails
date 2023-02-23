3558a6fba1c77f9d7ef95af96ecc9fc316295d66
teruteru <tutituti3080@gmail.com>
2023-02-23 13:17:11 +0900

    Remove: 質問に関するモデルの削除

--- app/models/unknown_flower.rb
+++
@@ -1,9 +0,0 @@
-class UnknownFlower < ApplicationRecord
-  mount_uploader :image, ImageUploader
-
-  belongs_to :user
-  has_many :answers, dependent: :destroy
-  has_many :receptions, dependent: :destroy
-
-  validates :feature, presence: true, length: { maximum: 50000 }
-end
